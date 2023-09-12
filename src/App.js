14c5b69765bcd273e22742f57c08eb7d9cb477c4
CalebKabaya <calebmutuku4@gmail.com>
2023-09-12 22:46:55 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { HelmetProvider } from 'react-helmet-async';
 import Router from './routes';
 // theme
 import ThemeProvider from './theme';
+import './index.css';
+
 // components
 import { StyledChart } from './components/chart';
 import ScrollToTop from './components/scroll-to-top';
