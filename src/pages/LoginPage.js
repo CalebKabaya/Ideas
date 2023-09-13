7b81004fd923e4c28e02a5ccd2de14ad3370f339
CalebKabaya <calebmutuku4@gmail.com>
2023-09-13 14:56:13 +0300

    updates

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,107 +1,58 @@
-import { Helmet } from 'react-helmet-async';
-// @mui
-import { styled } from '@mui/material/styles';
-import { Link, Container, Typography, Divider, Stack, Button } from '@mui/material';
-// hooks
-import useResponsive from '../hooks/useResponsive';
-// components
-import Logo from '../components/logo';
-import Iconify from '../components/iconify';
-// sections
-import { LoginForm } from '../sections/auth/login';
+import React from 'react';
+import styled from 'styled-components';
+import Logo1 from './logo2-removebg-preview.png'
+import './login.css';
 
-// ----------------------------------------------------------------------
 
-const StyledRoot = styled('div')(({ theme }) => ({
-  [theme.breakpoints.up('md')]: {
-    display: 'flex',
-  },
-}));
 
-const StyledSection = styled('div')(({ theme }) => ({
-  width: '100%',
-  maxWidth: 480,
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  boxShadow: theme.customShadows.card,
-  backgroundColor: theme.palette.background.default,
-}));
+function App() {
+  return  <MainContainer>
+    <img className='login--logo' src={Logo1} alt='Britam Logo'/>
 
-const StyledContent = styled('div')(({ theme }) => ({
-  maxWidth: 480,
-  margin: 'auto',
-  minHeight: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  flexDirection: 'column',
-  padding: theme.spacing(12, 0),
-}));
+    <h2 className='welcome--text'>Welcome to ideas Portal</h2>
+    <h3 className='logindesc'>Please enter your details</h3>
+<div className='wrapper'>
+    <form action='#'>
+      <div className='input-box'>
+        <input type='text' placeholder='Enter your email'/>
+      </div>
+      <div className='input-box'>
+        <input type='password' placeholder='Enter your password'/>
+      </div>
+      <div className='checker'>
+    <input type="checkbox" id="Remember me" name="Remember me"  />
+    <label htmlFor="Rememeber me">Remember me for 30 days?</label>
+  </div>
+  <div className=''>
+        <button className='login--button' type='submit' value='Register now'>
+        sign in
+        </button>
+      </div>
+      
+             </form>
 
-// ----------------------------------------------------------------------
+    </div>
 
-export default function LoginPage() {
-  const mdUp = useResponsive('up', 'md');
+    
 
-  return (
-    <>
-      <Helmet>
-        <title> Login | Minimal UI </title>
-      </Helmet>
 
-      <StyledRoot>
-        <Logo
-          sx={{
-            position: 'fixed',
-            top: { xs: 16, sm: 24, md: 40 },
-            left: { xs: 16, sm: 24, md: 40 },
-          }}
-        />
-
-        {mdUp && (
-          <StyledSection>
-            <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-              Hi, Welcome Back
-            </Typography>
-            <img src="/assets/illustrations/illustration_login.png" alt="login" />
-          </StyledSection>
-        )}
-
-        <Container maxWidth="sm">
-          <StyledContent>
-            <Typography variant="h4" gutterBottom>
-              Sign in to Minimal
-            </Typography>
-
-            <Typography variant="body2" sx={{ mb: 5 }}>
-              Don’t have an account? {''}
-              <Link variant="subtitle2">Get started</Link>
-            </Typography>
-
-            <Stack direction="row" spacing={2}>
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:google-fill" color="#DF3E30" width={22} height={22} />
-              </Button>
-
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:facebook-fill" color="#1877F2" width={22} height={22} />
-              </Button>
-
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={22} height={22} />
-              </Button>
-            </Stack>
-
-            <Divider sx={{ my: 3 }}>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                OR
-              </Typography>
-            </Divider>
-
-            <LoginForm />
-          </StyledContent>
-        </Container>
-      </StyledRoot>
-    </>
-  );
+  </MainContainer>
+    
 }
+
+const MainContainer = styled.div`
+
+display: flex;
+align-items: center;
+flex-direction: column;
+height: 55vh;
+width: 50vh;
+border-radius: 12px;
+background: rgba(102, 96, 96, 0.65);
+box-shadow: 0px 1px 2px 0px rgba(16, 24, 40, 0.06), 0px 1px 3px 0px rgba(16, 24, 40, 0.10);
+`;
+
+
+
+
+export default App;
