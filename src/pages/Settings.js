f40729a8108e880c0282efd0ed846ab472512920
CalebKabaya <calebmutuku4@gmail.com>
2023-09-18 16:17:34 +0300

    fixed form error

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,26 +1,15 @@
 import { Helmet } from 'react-helmet-async';
 // @mui
-import { Grid, Button, Container, Stack, Typography } from '@mui/material';
+import { Container, Stack, Typography } from '@mui/material';
 // components
-import Iconify from '../components/iconify';
-import { BlogPostCard, BlogPostsSort, BlogPostsSearch } from '../sections/@dashboard/blog';
 // mock
-import POSTS from '../_mock/blog';
 // import BasicTabs from '../../src/sections/settings/settings';
 import BasicTabs from '../sections/settings/settings';
 
 
 
 
-// ----------------------------------------------------------------------
 
-const SORT_OPTIONS = [
-  { value: 'latest', label: 'Latest' },
-  { value: 'popular', label: 'Popular' },
-  { value: 'oldest', label: 'Oldest' },
-];
-
-// ----------------------------------------------------------------------
 
 export default function BlogPage() {
   return (
